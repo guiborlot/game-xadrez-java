93e7bb2c568aa360da927562c8f3e3bab21ad033
GUILHERME BORLOT OLIVEIRA <guiborlot@edu.unifil.br>
2020-09-19 13:02:14 -0300

    first class position

--- src/applicattion/Program.java
+++ src/applicattion/Program.java
@@ -1,9 +1,12 @@
 package applicattion;
 
+import boardgame.Position;
+
 public class Program {
 
-	public static void main(String[] args) {			
-		System.out.println("Hello World");
+	public static void main(String[] args) {		
+		
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 	}
-
 }
